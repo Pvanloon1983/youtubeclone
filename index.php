2c6ea64f8ffcbdccef82326e2400185522d6a579
Bilal van Loon <vanloon_1983@hotmail.com>
2023-08-26 01:11:10 +0200

    Added some HTML andf stylings for navbar and sidebar

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>YouTube Clone</title>
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
 <body>
-  <h1>Hello World!</h1>
-</body>
+  <div id="pageContainer">
+    <div id="mastHeadContainer">
 
+    </div>
+    <div id="sideNavContainer">
+
+    </div>
+    <div id="mainSectionContainer">
+      <div id="mainContentContainer">
+        hello mate
+      </div>
+    </div>
+  </div>
+</body>
 </html>
